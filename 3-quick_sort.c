735b7d20c9d6444c5f99da9d4164547726a2efb9
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-01-23 08:27:20 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -36,7 +36,7 @@ int lomuto_partition(int *array, int low, int high, size_t size)
         pivot = array[high];
         i = low - 1;
 
-        for (j = low, i; j <= high - 1; j++)
+        for (j = low; j <= high - 1; j++)
         {
                 if (array[j] <= pivot)
                 {
