16d60b7d55a36d3baf5b9721a5ac8e599af9a08a
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-01-23 10:00:30 +0300

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,1 +1,64 @@
+#include "sort.h"
 
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the head of the doubly linked list
+ * @a: First node to swap
+ * @b: Second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t *a, listint_t *b)
+{
+    if (a->prev)
+        a->prev->next = b;
+    else
+        *list = b;
+
+    if (b->next)
+        b->next->prev = a;
+
+    a->next = b->next;
+    b->prev = a->prev;
+    a->prev = b;
+    b->next = a;
+}
+/**
+ * cocktail_sort_list - Sorts a doubly linked list using Cocktail Shaker Sort
+ * @list: Pointer to the head of the doubly linked list
+ */
+void cocktail_sort_list(listint_t **list)
+{
+    int swapped;
+    listint_t *current;
+
+    if (list == NULL || *list == NULL)
+        return;
+
+    do {
+        swapped = 0;
+        current = *list;
+
+        while (current->next != NULL) {
+            if (current->n > current->next->n) {
+                swap_nodes(list, current, current->next);
+                print_list(*list);
+                swapped = 1;
+            }
+            else
+                current = current->next;
+        }
+
+        if (!swapped)
+            break;
+
+        swapped = 0;
+        while (current->prev != NULL) {
+            if (current->n < current->prev->n) {
+                swap_nodes(list, current->prev, current);
+                print_list(*list);
+                swapped = 1;
+            }
+            else
+                current = current->prev;
+        }
+    } while (swapped);
+}
