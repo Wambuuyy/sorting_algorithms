f0216fa964dbf55047af09fff59043a515ebcfd1
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-01-23 09:49:59 +0300

    swap under certain condition

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -48,9 +48,9 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 			}
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	if (i + 1 != high)
+	if (array[i + 1] != array[high])
 	{
+		swap(&array[i + 1], &array[high]);
 		print_array(array, size);
 	}
 	return (i + 1);
