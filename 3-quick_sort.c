93f343f3f9965010d9eef659f12f52efb3c368c7
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-23 08:33:42 +0300

    fix betty errors on quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,11 +9,11 @@
  */
 void swap(int *a, int *b)
 {
-        int temp;
-	
+	int temp;
+
 	temp = *a;
-        *a = *b;
-        *b = temp;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -31,15 +31,15 @@ void swap(int *a, int *b)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-        int pivot, i, j;
+	int pivot, i, j;
 
-        pivot = array[high];
-        i = low - 1;
+	pivot = array[high];
+	i = low - 1;
 
-        for (j = low; j <= high - 1; j++)
-        {
-                if (array[j] <= pivot)
-                {
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
 			i++;
 			if (i != j)
 			{
@@ -47,13 +47,13 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 				print_array(array, size);
 			}
 		}
-        }
+	}
 	swap(&array[i + 1], &array[high]);
 	if (i + 1 != high)
 	{
 		print_array(array, size);
 	}
-        return (i + 1);
+	return (i + 1);
 }
 /**
  * quick_sort_recursive - a quick sort helper function that handles recursion
@@ -67,8 +67,8 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 	int pivot;
 
-        if (low < high)
-        {
+	if (low < high)
+	{
 		pivot = lomuto_partition(array, low, high, size);
 		quick_sort_recursive(array, low, pivot - 1, size);
 		quick_sort_recursive(array, pivot + 1, high, size);
@@ -82,7 +82,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-        if (array == NULL || size < 2)
-                return;
-        quick_sort_recursive(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_recursive(array, 0, size - 1, size);
 }
