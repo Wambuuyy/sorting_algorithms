e3d9c986614fd6d73e4ad48360726c7e86cf4939
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-01-27 23:12:57 +0300

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -29,29 +29,29 @@ int getMax(int *array, size_t size)
 void countingSort(int *array, size_t size, int exp)
 {
     const int radix = 10; /* The base of the number system*/
-    int *output = malloc(size * sizeof(int)), count, i;
-    size_t i, j, k, l;
+    int *output = malloc(size * sizeof(int)), count, j;
+    size_t i, k, l;
     
     if (output == NULL)
         return;
 
     count[radix + 1] = {0};
 
-    // Count the occurrences of each digit at the current place value
+    /* Count the occurrences of each digit at the current place value*/
     for (i = 0; i < size; i++)
         count[(array[i] / exp) % radix]++;
 
-    // Accumulate the counts to get the correct position of each element
+    /* Accumulate the counts to get the correct position of each element*/
     for (j = 1; j < radix; j++)
         count[j] += count[j - 1];
 
-    // Build the output array using the counting array and update counts
+    /* Build the output array using the counting array and update counts*/
     for (k = size - 1; k >= 0; k--) {
         output[count[(array[k] / exp) % radix] - 1] = array[k];
         count[(array[k] / exp) % radix]--;
     }
 
-    // Copy the sorted elements back to the original array
+    /* Copy the sorted elements back to the original array*/
     for (l = 0; l < size; l++)
         array[l] = output[l];
 
